7ebdda53dcd34b39d97ba3bd3b5132229fd0e1c9
Wojtek Przechodzeń <wprzecho@gmail.com>
2020-11-21 11:21:23 +0100

    06 refactoring

--- exercises/06-tdd-demo.spec.js
+++ exercises/06-tdd-demo.spec.js
@@ -1,57 +1,40 @@
 describe.skip('fizz buzz', () => {
-    // DEMO
-    it('return 1 for n = 1', () => {
-        const result = fizzBuzz(1);
-
-        expect(result).toEqual(1);
-    });
-
-    it('return 2 for n = 2', () => {
-        const result = fizzBuzz(2);
-
-        expect(result).toEqual(2);
-    });
-
-    it('return Fizz for n = 3', () => {
+    it('should return Fizz for n % 3', () => {
         const result = fizzBuzz(3);
 
         expect(result).toEqual('Fizz');
     });
 
-    it('return Buzz for n = 5', () => {
+    it('return Buzz for n % 5', () => {
         const result = fizzBuzz(5);
 
         expect(result).toEqual('Buzz');
     });
 
-    it('return Fizz for n = 6', () => {
-        const result = fizzBuzz(6);
-
-        expect(result).toEqual('Fizz');
-    });
-
-    it('return Buzz for n = 10', () => {
-        const result = fizzBuzz(10);
-
-        expect(result).toEqual('Buzz');
-    });
-
-    it('return FizzBuzz for n = 15', () => {
+    it('return FizzBuzz for n % 3 and n % 5', () => {
         const result = fizzBuzz(15);
 
         expect(result).toEqual('FizzBuzz');
+    });
+
+    it('should return n for n not divided by 3 and 5', () => {
+        const result = fizzBuzz(2);
+
+        expect(result).toEqual(2);
     });
 });
 
 const fizzBuzz = (n) => {
     const dividedBy3 = n % 3 === 0;
     const dividedBy5 = n % 5 === 0;
-    return dividedBy3 && dividedBy5
-      ? 'FizzBuzz'
-      : dividedBy3
-        ? 'Fizz'
-        : dividedBy5
-          ? 'Buzz'
-          : n;
+    if (dividedBy3 && dividedBy5) {
+        return 'FizzBuzz';
+    } else if (dividedBy3) {
+        return 'Fizz';
+    } else if (dividedBy5) {
+        return 'Buzz';
+    } else {
+        return n;
+    }
 }
 
